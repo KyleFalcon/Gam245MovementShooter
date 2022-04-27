ff1411f932cfbb0043a0924a9dd70bd6299b6a56
DanCast531 <dancast531@gmail.com>
2022-04-27 13:14:16 -0500

    weapon switching

--- Assets/Scripts/gun.cs
+++ Assets/Scripts/gun.cs
@@ -4,15 +4,21 @@ public class gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire"))
+        if (Input.GetButton("Fire") && Time.time >=nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -31,6 +37,15 @@ public class gun : MonoBehaviour
             {
                 target.TakeDamage(damage);           
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1.5f);
         }
     }
 
