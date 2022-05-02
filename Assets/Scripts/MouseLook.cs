8358ff735f3a9505e216148681321820fba84eb1
Kyle Falcon <falconkyle4@gmail.com>
2022-05-02 03:02:42 -0500

    More movement scripting

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,13 +6,16 @@ public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
 
-    public Transform playerBody;
+    public Transform orientation;
 
     float xRotation = 0f;
+    float yRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -21,12 +24,13 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        yRotation += mouseX;
+
         xRotation += mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        playerBody.Rotate(Vector3.up *mouseX);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
--- Assets/Scripts/PlayerMovementAdvanced.cs
+++ Assets/Scripts/PlayerMovementAdvanced.cs
@@ -4,15 +4,172 @@ using UnityEngine;
 
 public class PlayerMovementAdvanced : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    public float dashForce;
+    public float dashCooldown;
+    bool readyToDash;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode dashKey = KeyCode.LeftShift;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask ground;
+    bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    
+    public bool slopeCheck;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        readyToDash = true;
+        readyToJump = true;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //grounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+
+        MyInput();
+        SpeedControl();
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+            ResetJump();
+        }
+        else
+        {
+            rb.drag = 0;
+            readyToJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+
+            //Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+        if (Input.GetKeyDown(dashKey) && readyToDash)
+        {
+            readyToDash = false;
+
+            Dash();
+            Invoke(nameof(ResetDash), dashCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        //Movement Direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        slopeCheck = OnSlope();
+        if (OnSlope()) { 
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 40f, ForceMode.Force);
+
+            if(rb.velocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
+
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        rb.useGravity = !OnSlope();
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void Dash()
+    {
+        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        //rb.velocity = Vector3.zero;
+
+        rb.AddForce(inputDirection.normalized * dashForce, ForceMode.Impulse);
+    }
+    private void ResetDash()
+    {
+        readyToDash = true;
+    }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, (playerHeight * 0.5f) + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
